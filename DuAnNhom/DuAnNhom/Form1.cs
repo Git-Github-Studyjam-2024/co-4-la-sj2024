e4fe5a6875eb8f4f2765761e221b18feed5e6441
MinhTrung9443 <minhtrungbttv@gmail.com>
2024-02-28 13:02:09 +0700

    update home

--- DuAnNhom/DuAnNhom/Form1.cs
+++ DuAnNhom/DuAnNhom/Form1.cs
@@ -6,5 +6,20 @@ namespace DuAnNhom
         {
             InitializeComponent();
         }
+
+        private void homeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
     }
 }
